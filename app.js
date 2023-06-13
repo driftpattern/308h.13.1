8c4dd7b74ac8b88de852d90ec99f26afaa177cba
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-06-12 22:19:40 -0700

    imported code from 308h.12.1

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-// https://ps-react-curriculum.herokuapp.com/308H/13/lab/
-// R-ALAB 308H.13.1 - DOM Manipulation (Part Two)
+// https://ps-react-curriculum.herokuapp.com/308H/12/lab/
+// R-ALAB 308H.12.1 - DOM Manipulation (Part One)
 
+
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
+
+
+// Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.querySelector('#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+
+// Iterate over the entire menuLinks array
+menuLinks.forEach(function(link) {
+    var linkEl = document.createElement('a');
+    linkEl.href = link.href;
+    linkEl.textContent = link.text;
+    topMenuEl.appendChild(linkEl);
+  });
 
